52d5030543dcc4abd49eb137027d41fa11842cbd
Oikaze <102778036+oikazeHiro@users.noreply.github.com>
2023-01-06 14:39:17 +0800

    改回mysql

--- shiro_test/api/src/main/java/com/oik/api/config/MybatisPlusConfig.java
+++ shiro_test/api/src/main/java/com/oik/api/config/MybatisPlusConfig.java
@@ -12,7 +12,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.SQL_SERVER));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
 
